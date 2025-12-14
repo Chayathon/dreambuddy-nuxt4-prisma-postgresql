87649d7406c3a645f24f67996c7c4dde098d86a3
Chayathon.T <gusgus157903@gmail.com>
2025-12-14 20:51:25 +0700

    Setup Google Fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,24 @@ export default defineNuxtConfig({
     compatibilityDate: "2025-07-15",
     devtools: { enabled: true },
     css: ["./app/assets/css/main.css"],
-    modules: ["@nuxt/ui"],
+    modules: [
+        "@nuxt/ui",
+        [
+            "@nuxtjs/google-fonts",
+            {
+                families: {
+                    Inter: "200..700",
+                    Anuphan: ["400", "500"],
+                },
+                display: "swap",
+                preload: true,
+                prefetch: true,
+                preconnect: true,
+                download: true,
+                inject: true,
+            },
+        ],
+    ],
     vite: {
         plugins: [tailwindcss()],
     },
