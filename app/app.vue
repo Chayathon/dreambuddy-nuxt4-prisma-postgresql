6bf9cd493ecae754cf76ffb712da42095b31cb09
Chayathon.T <gusgus157903@gmail.com>
2025-12-15 19:22:25 +0700

    Created auth layout and pages with i18n support

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,20 @@
+<script setup lang="ts">
+const { $switchLocale, $getLocale } = useI18n();
+
+onMounted(() => {
+    if (import.meta.client) {
+        const savedLocale = localStorage.getItem("dreambuddy-locale");
+        if (savedLocale && savedLocale !== $getLocale()) {
+            $switchLocale(savedLocale);
+        }
+    }
+});
+</script>
+
 <template>
-  <UApp>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-  </UApp>
+    <UApp>
+        <NuxtLayout>
+            <NuxtPage />
+        </NuxtLayout>
+    </UApp>
 </template>
